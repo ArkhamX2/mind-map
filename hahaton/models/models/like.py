e2fc400dc4359c833a83fb37567753a8149dfced
S1stemus <islamovm988@gmail.com>
2024-09-21 13:53:24 +0300

    like model

---
+++ hahaton/models/models/like.py
@@ -0,0 +1,9 @@
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+from models.models.users import Users
+class Like(models.Model):
+    user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE) 
+    object_id = models.PositiveIntegerField()
+    object= GenericForeignKey('content_type', 'object_id')
