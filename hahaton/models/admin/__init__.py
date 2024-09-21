900487d237eaabeb3346641086c18143b3527b7b
S1stemus <islamovm988@gmail.com>
2024-09-21 15:56:19 +0300

    admin setup

---
+++ hahaton/models/admin/__init__.py
@@ -0,0 +1,7 @@
+from models.admin.users_admin import UserAdmin
+from models.admin.course_admin import CourseAdmin
+from models.admin.like_admin import LikeAdmin
+from models.admin.comment_admin import CommentAdmin
+from models.admin.project_admin import ProjectAdmin
+from models.admin.usertag_admin import UserTagAdmin
+from models.admin.coursetag_admin import CourseTagAdmin
