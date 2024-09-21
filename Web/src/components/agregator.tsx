deba766a874baa854ce26bdfbb673260c01b183b
PolShestogo <99017989+PolShestogo@users.noreply.github.com>
2024-09-21 20:03:19 +0300

    dota 2 gfl;

--- Web/src/components/agregator.tsx
+++ Web/src/components/agregator.tsx
@@ -6,9 +6,9 @@ import AgrCard from "../components/agrCard"
 const Agregator: FC = () => {
     return (
         <main style={{ backgroundColor: "#FFFFFF", width: '100%', height: '100%', padding: "15px" }}>
-            <div>
+            <div style={{display:'flex', flexDirection:'column'}}>
                 <div className="searchcontainer" style={{ width: "100%", height: '100%', display: "flex", flexDirection: 'column' }}>
-                    <div className="searchbarcontainer" style={{ display: 'inline-flex', justifyContent: 'space-between' }}>
+                    <div className="searchbarcontainer" style={{ display: 'inline-flex', justifyContent: 'space-between', alignSelf:'center' }}>
                         <div className="searchbar" style={{ justifySelf: 'start' }}>
                             <input type="text" style={{
                                 width: '800px', height: '56px', borderRadius: '10px', backgroundColor: '#F5F5F5',
@@ -16,18 +16,18 @@ const Agregator: FC = () => {
                             }}></input>
                         </div>
                         <div style={{display:'inline-flex'}}>
-                            <div className="searchButton" style={{ width: '56px', height: '56px', backgroundColor: '#EBEBFF', marginRight:'10px', textAlign:'center', borderRadius:'10px' }}>Поиск</div>
-                            <div className="filterbutton" style={{ width: '56px', height: '56px', backgroundColor: '#EBEBFF', borderRadius:'10px',  }}>Фильтр</div>
+                            <div className="searchButton" style={{ width: '56px', height: '56px', backgroundColor: '#EBEBFF', margin:'0 10px', textAlign:'center', borderRadius:'10px' }}>Поиск</div>
+                            <div className="filterbutton" style={{ width: '56px', height: '56px', backgroundColor: '#EBEBFF', borderRadius:'10px'  }}>Фильтр</div>
                         </div>
 
                     </div>
-                    <div className="tagscontainer" style={{ display: 'flex', flexDirection: 'row' }}>
+                    <div className="tagscontainer" style={{ display: 'flex', flexDirection: 'row', alignSelf:'center' }}>
                     <div style={{
                                         height: '32px', backgroundColor: '#EBEBFF', display: 'inline-block', padding: '0 15px', textAlign: 'center', borderRadius: '10px',
                                         fontSize:'20px', fontWeight:'400', margin:'5px'}}>тэг</div>
                     </div>
                 </div>
-                <div className="cardContainer" style={{ display: 'grid', overflowY: 'auto', gridTemplateColumns: '1fr 1fr', columnGap: '10px' }}>
+                <div className="cardContainer" style={{ display: 'grid', overflowY: 'auto', gridTemplateColumns: '1fr 1fr', columnGap: '10px', alignSelf:'center' }}>
                     <AgrCard></AgrCard>
                     <AgrCard></AgrCard>
                     <AgrCard></AgrCard>
