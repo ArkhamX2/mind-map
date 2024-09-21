1da08049913893d0c865e27569187a3c71f5c9c7
PolShestogo <99017989+PolShestogo@users.noreply.github.com>
2024-09-21 16:34:12 +0300

    profile demo

--- Web/src/components/profile.tsx
+++ Web/src/components/profile.tsx
@@ -12,21 +12,26 @@ const Profile: FC = () => {
                     <div className="descrcontainer" style={{margin:'10px'}}>
                         <div className="name" style={{margin:'10px'}}><h2>Имя Фамилия</h2></div>
                         <div className="descr" style={{margin:'15px 10px', backgroundColor:'#EEEFFF', width:'800px', height:"240px", borderRadius:'5px'}}>
-                            <div style={{margin:"10px", padding:'5px'}}><h3>О себе:</h3></div>
+                            <div className="label" style={{margin:"10px", padding:'5px'}}><h3>О себе:</h3></div>
                             Текст
                         </div>
                     </div>
                 </div>
-                <div className="tagscontainer">
-                    <div className="label">Теги:</div>
-                    <div className="taglist">
-                        <div>
+                <div className="tagscontainer"  style={{ width: "100%", height: '100%', display: "flex", flexDirection: 'column', padding:'10px' }}>
+                    <div className="label" style={{margin:'5px 0'}}><h2>Теги</h2></div>
+                    <div className="taglist" style={{border:'2px solid lightgray', width:"1130px", height:"400px"}}>
+                        <div style={{display:'flex', flexDirection:'column'}}>
+                            Объект <br/>
+                            Объект <br/>
+                            Объект <br/>
+                            Объект <br/>
                         </div>
-                        <button style={{width:'160px', height:'42px'}}>текст</button>
+                        <button style={{width:'150px', height:'38px'}}>текст</button>
                     </div>
                 </div>
-                <div className="mapcontainer">
-                    <div className="label">Карта знаний</div>
+                <div className="mapcontainer" style={{width: "100%", height: '100%', display: "flex", flexDirection: 'column', padding:'10px'}}>
+                    <div className="label" style={{margin:'5px 0'}}><h2>Карта знаний</h2></div>
+                    <div style={{display:'flex', height:'600px', width:'1130px', border:'2px solid lightgray' }}></div>
                 </div>
             </div>
         </main>
