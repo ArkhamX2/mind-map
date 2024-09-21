51ae3a5dc4bc45d0ba43b3b5f1d4fa5cc01d7316
PolShestogo <99017989+PolShestogo@users.noreply.github.com>
2024-09-21 16:02:11 +0300

    prof css

--- Web/src/components/login.tsx
+++ Web/src/components/login.tsx
@@ -4,63 +4,64 @@ import { Link, useNavigate } from 'react-router-dom'
 import { useAppDispatch } from "../utility/hooks";
 
 const Login: FC = () => {
-    const dispatch = useAppDispatch()
-    const navigate = useNavigate();
-    const [data, setData] = useState({
-        email: "",
-        password: "",
-    });
-    const [errorText, setErrorText] = useState<string>("");
-    const login = () => {
-        axios.post("http://127.0.0.1:8000/security/login/", {
-            withCredentials: true,
-            email: data.email,
-            password: data.password
-        }).then((res) => {
-            setErrorText("")
-            console.log("Server response: ", res);
-            //Cookies.set('trains_cookie', res.data.access);
-            navigate("/trains")
-        }).catch((err) => {
-            console.log("Server respondend with error: ", err);
-            switch (err.status)
-            {
-            case 400:
-                setData({ ...data, password:""})
-                setErrorText("Введена неверная почта/пароль")
-                break
-            }
-        })
-    }
-    return (
-        <div className="container">
-            <div className="header">
-                <div className="text">Login</div>
-            </div>
-            <div className="inputs">
-                <div className="input">
-                    <input value={data.email} onChange={(e) => setData({ ...data, email: e.target.value })} placeholder="email" type="email"></input>
-                </div>
-                <div className="input">
-                    <input value={data.password} onChange={(e) => setData({ ...data, password: e.target.value })} placeholder="password" type="password"></input>
-                </div>
-            </div>
-            <div>
-                {errorText != "" ?
-                    <>
-                        <label style={{ color: 'red' }}>{errorText}</label>
-                    </>
-                    :
-                    <>
+        const dispatch = useAppDispatch()
+        const navigate = useNavigate();
+        const [data, setData] = useState({
+                email: "",
+                password: "",
+        });
+        const [errorText, setErrorText] = useState<string>("");
+        const login = () => {
+                axios.post("http://127.0.0.1:8000/security/login/", {
+                        withCredentials: true,
+                        email: data.email,
+                        password: data.password
+                }).then((res) => {
+                        setErrorText("")
+                        console.log("Server response: ", res);
+                        //Cookies.set('trains_cookie', res.data.access);
+                        navigate("/trains")
+                }).catch((err) => {
+                        console.log("Server respondend with error: ", err);
+                        switch (err.status) {
+                                case 400:
+                                        setData({ ...data, password: "" })
+                                        setErrorText("Введена неверная почта/пароль")
+                                        break
+                        }
+                })
+        }
+        return (
+                <main>
+                        <div className="container">
+                                <div className="header">
+                                        <div className="text">Login</div>
+                                </div>
+                                <div className="inputs">
+                                        <div className="input">
+                                                <input value={data.email} onChange={(e) => setData({ ...data, email: e.target.value })} placeholder="email" type="email"></input>
+                                        </div>
+                                        <div className="input">
+                                                <input value={data.password} onChange={(e) => setData({ ...data, password: e.target.value })} placeholder="password" type="password"></input>
+                                        </div>
+                                </div>
+                                <div>
+                                        {errorText != "" ?
+                                                <>
+                                                        <label style={{ color: 'red' }}>{errorText}</label>
+                                                </>
+                                                :
+                                                <>
 
-                    </>}
-            </div>
-            <div className="submit-container">
-                <button onClick={() => navigate("/registration")} className="submit">Sign Up</button>
-                <button onClick={() => login()} className="submit">Login</button>
-            </div>
-        </div>
-    )
+                                                </>}
+                                </div>
+                                <div className="submit-container">
+                                        <button onClick={() => navigate("/registration")} className="submit">Sign Up</button>
+                                        <button onClick={() => login()} className="submit">Login</button>
+                                </div>
+                        </div>
+                </main>
+        )
 }
 
 export default Login
--- Web/src/components/profile.tsx
+++ Web/src/components/profile.tsx
@@ -4,14 +4,32 @@ import { Link, useNavigate } from 'react-router-dom'
 
 const Profile: FC = () => {
     return (
-        <div className="container">
-            <div className="header">
-                <div className="text">Profile</div>
+        <main style={{ backgroundColor: "#F5F5F5" }}>
+            <div style={{margin:'0 160px'}}>
+                <div className="profileinfo" style={{ width: "100%", height: '100%', display: "flex", flexDirection: 'row' }}>
+                    <div className="logo" style={{ width: '300px', height: '300px', backgroundColor: 'lightgray', borderRadius: '15px', margin:'10px 15px' }}>
+                    </div>
+                    <div className="descrcontainer" style={{margin:'10px'}}>
+                        <div className="name" style={{margin:'10px'}}><h2>Имя Фамилия</h2></div>
+                        <div className="descr" style={{margin:'15px 10px', backgroundColor:'#EEEFFF', width:'800px', height:"240px", borderRadius:'5px'}}>
+                            <div style={{margin:"10px", padding:'5px'}}><h3>О себе:</h3></div>
+                            Текст
+                        </div>
+                    </div>
+                </div>
+                <div className="tagscontainer">
+                    <div className="label">Теги:</div>
+                    <div className="taglist">
+                        <div>
+                        </div>
+                        <button style={{width:'160px', height:'42px'}}>текст</button>
+                    </div>
+                </div>
+                <div className="mapcontainer">
+                    <div className="label">Карта знаний</div>
+                </div>
             </div>
-            <div className="inputs">
-                
-            </div>
-        </div>
+        </main>
     )
 }
 
