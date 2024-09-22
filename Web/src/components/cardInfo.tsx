65dd3b2af2732a37ebe05ddcab58134b53524f70
PolShestogo <99017989+PolShestogo@users.noreply.github.com>
2024-09-22 12:18:09 +0300

    фывфвфы

--- Web/src/components/cardInfo.tsx
+++ Web/src/components/cardInfo.tsx
@@ -1,9 +1,12 @@
 import { FC, useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 
-const ProjectCard: FC = () => {
+const CardInfo: FC = () => {
         return (
-                <div className="card" style={{ display: 'flex', width: '650px', height: '610px', backgroundColor: '#F3F3F8', flexDirection: 'column', padding: '15px', borderRadius: '10px', margin: '5px' }}>
+                <div className="card" style={{
+                        display: 'flex', width: '650px', height: '1300px', backgroundColor: '#F3F3F8', flexDirection: 'column',
+                        padding: '15px', borderRadius: '10px', margin: '5px', overflowY: 'auto'
+                }}>
                         <div className="image" style={{ backgroundColor: 'lightgray', width: '100%', height: '70%', margin: '0 0 10px 0', borderRadius: '10px' }}></div>
                         <div className="Text" style={{ fontSize: "28px" }}>Абиоба боба фактор я крутецкий мэйн актор по локации прошлеся трактор в рейд поше лне оценив времени</div>
                         <div className='tagscontainer'>
@@ -15,10 +18,22 @@ const ProjectCard: FC = () => {
                         <div className="button"><button style={{
                                 width: '100%', height: '48px', margin: '5px 0', border: '0px transparent', backgroundColor: "#7D83FF",
                                 borderRadius: '10px', color: 'white', fontSize: '22px'
-                        }}>ПОДРОБНЕЕ...</button></div>
+                        }}>Просмотр</button></div>
+                        <div>
+                                <div className='comments'>
+                                        <div>Коменнтарии</div>
+                                        <div> коментарий 1</div>
+                                        <div style={{display:'flex', flexDirection:'row'}}>
+                                                <input></input>
+                                                <div>Отправить</div>
+                                        </div>
+
+
+                                </div>
+                        </div>
                 </div>
 
         )
 }
 
-export default ProjectCard
+export default CardInfo
