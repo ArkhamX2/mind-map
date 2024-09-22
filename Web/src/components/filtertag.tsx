15c02bfcd2671af05a1ab1571464857276c94e05
PolShestogo <99017989+PolShestogo@users.noreply.github.com>
2024-09-22 11:58:17 +0300

    props start

--- Web/src/components/filtertag.tsx
+++ Web/src/components/filtertag.tsx
@@ -2,10 +2,14 @@ import { FC, useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import classes from '../styles/login.module.css'
 
-const FilterTag: FC = () => {
+interface filtertagProps {
+        text?: string;
+        defaultValue?: string | number | readonly string[] | undefined;
+      }
+const FilterTag: FC<filtertagProps> =(props)=> {
     return (
         <div style={{width:'380spx', height:'42px', padding:'5px', display:'flex', flexDirection:'row', justifyContent:'space-between'}}>
-                <div style={{fontSize:'24px'}}>Название тега</div>
+                <div style={{fontSize:'24px'}}>{props.text}</div>
                 <div style={{width:'30px', height:'30px', border:'4px solid #8787C7'}}>Галочка</div>
         </div>
     )
--- Web/src/components/profile.tsx
+++ Web/src/components/profile.tsx
@@ -219,20 +219,22 @@ const testCourseTagData = [
 
 const Profile: FC = () => {
     return (
-        <main style={{ backgroundColor: "#FFFFFF"}}>
-            <div style={{ margin: '0 160px', overflowY:'auto'  }}>
-                <div className="profileinfo" style={{ width: "100%", height: '100%', display: "flex", flexDirection: 'row', marginTop:'70px' }}>
+        <main style={{ backgroundColor: "#FFFFFF", width: '100%', height: '100%', padding: "15px" }}>
+            <div style={{overflowY:'auto', display:'flex', flexDirection:'column', width: '100%', height: '100%'}}>
+                <div className="profileinfo" style={{display: "flex", flexDirection: 'row', marginTop:'70px', alignSelf:'center' }}>
                     <div className="logo" style={{ width: '300px', height: '300px', backgroundColor: 'lightgray', borderRadius: '15px', margin: '10px 15px' }}>
                     </div>
-                    <div className="descrcontainer" style={{ margin: '10px' }}>
+                    <div className="descrcontainer" style={{ margin: '10px', alignSelf:'center' }}>
                         <div className="name" style={{ margin: '10px' }}><h2>Имя Фамилия</h2></div>
                         <div className="descr" style={{ margin: '15px 10px', backgroundColor: '#EEEFFF', width: '800px', height: "240px", borderRadius: '5px' }}>
-                            <div className="label" style={{ margin: "10px 5px", padding: '5px' }}><h3>О себе:</h3></div>
-                            <textarea style={{ margin: '0 5px', padding: '0 5px', width: '97%', height: '78%', backgroundColor: '#EEEFFF', border: '0px #EEEFFF', outlineWidth: '0', resize: 'none', fontSize:'18px' }}></textarea>
+                            <div className="label" style={{ margin: "5px 5px", padding: '5px' }}><h3>О себе:</h3></div>
+                            <textarea style={{ margin: '0px 5px', padding: '0 5px', width: '97%', height: '78%', backgroundColor: '#EEEFFF', border: '0px #EEEFFF', outlineWidth: '0', resize: 'none', fontSize:'18px'
+                                , color:'#06146C', textDecoration:'#06146C'
+                             }}></textarea>
                         </div>
                     </div>
                 </div>
-                <div className="tagscontainer" style={{ width: "100%", height: '100%', display: "flex", flexDirection: 'column', padding: '10px' }}>
+                <div className="tagscontainer" style={{ width: "100%", height: '100%', display: "flex", flexDirection: 'column', padding: '10px', alignItems:'center'  }}>
                     <div className="label" style={{ margin: '5px 0' }}><h2>Теги</h2></div>
                     <div className="taglist" style={{ border: '2px solid lightgray', width: "1130px", height: "400px", overflowY: 'auto' }}>
                         <div style={{ display: 'flex', flexDirection: 'column' }}>
@@ -251,7 +253,7 @@ const Profile: FC = () => {
                         , border: '0px solid transparent', borderRadius: '10px'
                     }}>Добавить</button>
                 </div>
-                <div className="mapcontainer" style={{ width: "100%", height: '100%', display: "flex", flexDirection: 'column', padding: '10px' }}>
+                <div className="mapcontainer" style={{ width: "100%", height: '100%', display: "flex", flexDirection: 'column', padding: '10px', alignItems:'center' }}>
                     <div className="label" style={{ margin: '5px 0' }}><h2>Карта знаний</h2></div>
                     <div style={{ display: 'flex', height: '600px', width: '1130px', border: '2px solid lightgray' }}>
                         <MindNetwork props={createMindNerworkProps(testCourseData, testCourseTagData)} />
